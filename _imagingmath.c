f7b60691fa795a4f0b0db0a2d3980b662f771532
Ben Trofatter <bentrofatter@gmail.com>
2011-06-29 03:53:40 -0500

    Standardized on Py_RETURN_NONE when returning Py_None

--- _imagingmath.c
+++ _imagingmath.c
@@ -188,8 +188,7 @@ _unop(PyObject* self, PyObject* args)
     
     unop(out, im1);
 
-    Py_INCREF(Py_None);
-    return Py_None;
+    Py_RETURN_NONE;
 }
 
 static PyObject *
@@ -212,8 +211,7 @@ _binop(PyObject* self, PyObject* args)
     
     binop(out, im1, im2);
 
-    Py_INCREF(Py_None);
-    return Py_None;
+    Py_RETURN_NONE;
 }
 
 static PyMethodDef _functions[] = {
