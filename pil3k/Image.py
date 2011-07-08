6f7bb5516597d810904e38ab2c07f7b401010a8e
Ben Trofatter <bentrofatter@gmail.com>
2011-07-08 03:21:24 -0500

    Fixed some unseen bytes/unicode errors

--- pil3k/Image.py
+++ pil3k/Image.py
@@ -375,7 +375,7 @@ def _getdecoder(mode, decoder_name, args, extra=()):
         # get decoder
         decoder = getattr(core, decoder_name + "_decoder")
         # print(decoder, (mode,) + args + extra)
-        return decoder((mode,) + args + extra)
+        return decoder(*((mode,) + args + extra))
     except AttributeError:
         raise IOError("decoder {0} not available".format(decoder_name))
 
@@ -391,7 +391,7 @@ def _getencoder(mode, encoder_name, args, extra=()):
         # get encoder
         encoder = getattr(core, encoder_name + "_encoder")
         # print(encoder, (mode,) + args + extra)
-        return encoder((mode,) + args + extra)
+        return encoder(*((mode,) + args + extra))
     except AttributeError:
         raise IOError("encoder {0} not available".format(encoder_name))
 
